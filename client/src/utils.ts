9f4dd10b837ed1fbdc53b794ec21849d74c75df4
Chris Woodle <chris@droplit.io>
2019-04-27 11:12:48 -0500

    default to localhost:3000

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -8,7 +8,7 @@ export function blobToArrayBuffer(blob: Blob) {
 import axios, { AxiosResponse } from 'axios';
 export function getWavFile(fileName: string) {
     return axios({
-        url: `${process.env.VUE_APP_SERVICE_HOST}/${fileName}`,
+        url: `${process.env.VUE_APP_SERVICE_HOST || 'http://localhost:3000'}/${fileName}`,
         method: 'GET',
         responseType: 'blob',
     }).then((response: AxiosResponse<Blob>) => {
